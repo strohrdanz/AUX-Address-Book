a574a6894de82ba5e15eb9254b3940175bf69592
Stephanie <stephanie@rohrdanz.com>
2014-07-06 17:02:22 -0400

    add jquery

--- js/script.js
+++ js/script.js
@@ -1,104 +1,55 @@
-function getHTTPObject() {
+$(document).ready(function(){  
+    var addr = {
+        search : function(event){
+            event.preventDefault();
 
-    var xhr;
+            $.getJSON('data/contacts.json', function (data) {
 
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        xhr = new ActiveObject("Msxml2.XMLHTTP");
-    }
-    return xhr;
-}
+                var searchValue = $("#input").val();
+                addrBook = data.addressBook,
+                count = addrBook.length,
 
-function ajaxCall(dataURL, outputElement, callback) {
+                $('#output').empty();
 
-    var request = getHTTPObject();
-
-    outputElement.innerHTML = "Loading...";
-
-    request.onreadystatechange = function() {
-        if (request.readyState === 4 && request.status === 200) {
-            var contacts = JSON.parse(request.responseText); 
-            if (typeof callback === "function") {
-                callback(contacts);
-            } // end typeof check
-        } // end ajax status check
-    } // end readystatechange
-
-    request.open("GET", dataURL, true);
-    request.send(null);
-} // end ajaxCall
-
-(function(){
-
-var searchForm = document.getElementById("search"),
-    searchField = document.getElementById("input"),
-    getAllButton = document.getElementById("getall"),
-    target = document.getElementById("list");
-
-var addr = {
-    search : function(event){
-        var output = document.getElementById("output");
-
-        ajaxCall('data/contacts.json', output, function(data){
-        
-        var searchValue = searchField.value;
-            addrBook = data.addressBook,
-            count = addrBook.length,
-        i;
-
-        event.preventDefault();
-
-        target.innerHTML = "";
-
-        if(count > 0 && searchValue !== ""){
-            for(i = 0; i < count; i = i + 1) {
-                var obj = addrBook[i],
-                isItFound = obj.name.indexOf(searchValue);
-                if(isItFound !== -1) {
-                    target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>';
+                if (count > 0 && searchValue !== "") {
+                    $.each(addrBook, function (i, obj) {
+                        var isItFound = obj.name.indexOf(searchValue);
+                        if(isItFound !== -1) {
+                            $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
+                        }
+                    });
                 }
-            }
-        }
+            });
+        },
+        getAllContacts : function (){
+            $.getJSON('data/contacts.json', function (data) {
+                var addrBook = data.addressBook,
+                    count = addrBook.length;
+                $('#output').empty();
+                if (count > 0) {
+                    $.each(addrBook, function (i, obj) {
+                        $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
+                    });
+                }
+            })
+            .fail( function(d, textStatus, error) {
+                console.error(textStatus + " error: " + error);
+            }); 
+        } // end get all
+    } // end addr 
+
+    $("#input").keyup(addr.search).focus(function () {
+        $(this).parent().addClass("active");
+    }).blur(function () {
+        $(this).parent().removeClass("active");
     });
-    },
-    getAllContacts : function (){
-        
-        var output = document.getElementById("output");
 
-        ajaxCall('data/contacts.json', output, function(data){
-        
-        var addrBook = data.addressBook,
-            count = addrBook.length,
-            i;
+    $("#search").hover(function () {
+        $(this).addClass("hovering");
+    }, function () {
 
-        target.innerHTML = "";
-        if(count > 0){
-            for(i = 0; i < count; i = i + 1) {
-                var obj = addrBook[i];
-                target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>';
-            }
-        }
+        $(this).removeClass("hovering");
+    }).submit(addr.search);;
+
+    $("#getall").click(addr.getAllContacts);
     });
-    },
-    addActiveSession : function (){
-        this.parentNode.setAttribute("class","active");
-    },
-    removeActiveSession : function (){
-        this.parentNode.removeAttribute("class","active");
-    },
-    addHoverClass : function(){
-        searchForm.setAttribute("class","hovering");
-    },
-    removeHoverClass : function(){
-        searchForm.removeAttribute("class","hovering");
-    }
-}
-
-searchField.addEventListener("keyup", addr.search, false);
-
-getAllButton.addEventListener("click", addr.getAllContacts, false);
-
-searchField.addEventListener("submit",addr.search,false);
-
-})();
